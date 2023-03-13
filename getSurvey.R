6906f8f000608b713b08d3f51aba8ac1d9639ef0
joshkenney <joshuagkenney@gmail.com>
2023-03-13 14:46:31 -0400

    updated getSurvey.R to include better responseId fix

--- getSurvey.R
+++ getSurvey.R
@@ -19,30 +19,42 @@ getSurvey <- function(qualtrics) {
   # store qualtrics API credentials
   
   qualtRics::qualtrics_api_credentials(api_key = apiKey, 
-                                         base_url = baseUrl,
-                                         install = TRUE,
-                                         overwrite = TRUE)
+                                       base_url = baseUrl,
+                                       install = TRUE,
+                                       overwrite = TRUE)
   
   # Your original .Renviron will be backed up and stored in your R HOME directory if needed.
   # Your Qualtrics key and base URL have been stored in your .Renviron.  
   # To use now, restart R or run `readRenviron("~/.Renviron")`
   readRenviron("~/.Renviron")
-    
+  
   df <- qualtRics::fetch_survey(surveyID = toString(surveyIds[qualtrics]),
-                                 verbose = FALSE,
-                                 label = FALSE, # both of these must be set to false to import numeric
-                                 convert = FALSE, # both of these must be set to false to import numeric
-                                 force_request = TRUE)
+                                verbose = FALSE,
+                                label = FALSE, # both of these must be set to false to import numeric
+                                convert = FALSE, # both of these must be set to false to import numeric
+                                force_request = TRUE)
   
   return(df)
   
 }
 
 getResponseId <- function(qualtrics) {
- 
-  foo <- qualtrics %>% filter_all(any_vars(. %in% "40707"))
-  print(foo[c("ResponseId","src_subject_id","interview_age","phenotype","sex","site","subjectkey","Finished","Progress","StartDate","EndDate")])
+  
+  foo <- qualtrics %>% filter_all(any_vars(. %in% "NDARFC859JFD"))
+  print(foo[c("ResponseId","src_subject_id","interview_age","phenotype","sex","site","subjectkey","Finished","Progress","visit")])
+  responseId <- foo$ResponseId
+  src_subject_id <- foo$src_subject_id
+  interview_age <- foo$interview_age
+  phenotype <- foo$phenotype
+  sex <- foo$sex
+  site <- foo$site
+  subjectkey <- foo$subjectkey
+  visit <- foo$visit
   name <- deparse(substitute(qualtrics))
   print(surveyIds[name])
+  surveyId <- surveyIds[name]
+  write(paste(surveyId, responseId,src_subject_id,interview_age,phenotype,sex,site,subjectkey,visit,sep=','),                                            # Write new line to file
+        file = "my file.txt",
+        append = TRUE)
   
 }
