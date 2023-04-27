f6d69f9f08c8689fc3f008a678bd9dfe8370b6d8
joshkenney <joshuagkenney@gmail.com>
2023-04-26 20:23:05 -0400

    fixed formatting error in path write in createCsv

--- src/createCsv.R
+++ src/createCsv.R
@@ -1,5 +1,5 @@
 createCsv <- function(df) {
   path = paste('extracts/',deparse(substitute(df)),'.csv',sep='')
-  write.csv(df,paste(path,'.csv',sep=''),row.names = FALSE)
+  write.csv(df,path,row.names = FALSE)
   cat(paste0("Extract created at ",path))
 }
