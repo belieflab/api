2795139cd135d34b207dc31d0e688d80c71dbc7b
joshkenney <joshuagkenney@gmail.com>
2023-07-11 18:48:59 -0400

    added raceSummary() to getSurvey.R

--- getSurvey.R
+++ getSurvey.R
@@ -381,5 +381,58 @@ removeQualtricsDuplicates <- function(df) {
   } 
 }
 
+raceSummary <- function(demographics) {
+  # preserve original df
+  demo <- demographics
+  # select columns of interest
+  demo_clean <- tibble::tibble(demographics[, grepl("dem", names(demographics ))],
+                                     ethnicity = demographics$dem_hispanic
+  )
+  
+  
+  colnames(demo_clean) <- c("dem_race_asian",
+                                  "dem_race_alaskanative",
+                                  "dem_race_americanindian",
+                                  "dem_race_africanamerican",
+                                  "dem_race_caucasian",
+                                  "dem_race_hawaiian",
+                                  "dem_other",
+                                  "dem_ethnicity"
+  )
+  
+  # recode hispanic
+  demo_clean$dem_ethnicity <- ifelse(demo_clean$dem_ethnicity == 0,"not_hispanic_or_latino",
+                                           ifelse(demo_clean$dem_ethnicity == 1, "of_hispanic_or_latino",""))
+  
+  
+  demo_clean$count <- rowSums(as.data.frame(sapply(demo_clean[, grepl("race_", names(demo_clean))], as.numeric)), na.rm = TRUE)
+  
+  # if count = 1, then grab single race column
+  # else if count > 1, then label as "More Than One Race"
+  # else label as "Unknown or Not Reported"
+  
+  race_vector <- c()
+  
+  for (i in 1:nrow(demo_clean)){
+    race_vector[i] <- ifelse(demo_clean[i,]$count == 1,colnames(demo_clean)[1:6][which.max(demo_clean[i,1:6])],
+                             ifelse(demo_clean[i,]$count > 1, "more_than_one_race","unknwon_or_not_reported"))
+  }
+  
+  demo_clean$dem_race <- race_vector
+  
+  # add back in NDA required variables
+  demo_clean$src_subject_id <- demo$src_subject_id
+  demo_clean$subjectkey <- demo$subjectkey
+  demo_clean$interview_age <- demo$interview_age
+  demo_clean$interview_date <- demo$interview_date
+  demo_clean$ResponseId <- demo$ResponseId
+  demo_clean$phenotype <- demo$phenotype
+  demo_clean$sex <- demo$sex
+  demo_clean$site <- demo$site
+  
+  return(demo_clean)
+  
+}
+
 # create alias
 getQualtrics <- getSurvey
