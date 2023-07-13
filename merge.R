f735c843942b71bec43acae980d9b15445cc8480
tanya-tran <tanya_tran@urmc.rochester.edu>
2023-07-13 18:36:33 -0400

    merge.R working as expected, set all = false to exclude subjects missing trial data

--- merge.R
+++ merge.R
@@ -1,8 +1,6 @@
-source("clean/task/complete/mooney.R")
-source("clean/qualtrics/complete/rgpts.R")
-source("clean/qualtrics/complete/pss.R")
 
-multiMerge <- function(..., by = c("src_subject_id"), all = TRUE) {
+
+multiMerge <- function(..., by = c("src_subject_id","phenotype"), all = FALSE) {
   data_list <- list(...)
   
   # if in data_list:
@@ -12,9 +10,9 @@ multiMerge <- function(..., by = c("src_subject_id"), all = TRUE) {
   
   dfs <- Reduce(function(x, y) base::merge(x, y, by = by, all = all), data_list)
   View(dfs)
-  
+  unlink("extracts/dfs.csv")
+  createCsv(dfs)
 }
 
-multiMerge(mooney_clean, rgpts_clean, pss_clean)
 
 
