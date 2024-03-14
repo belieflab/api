fd88b7726912520f9e99cdd4bf5971613023b27a
trevorfwilliams~ <tfwillia@buffalo.edu~>
2024-03-13 22:40:21 -0500

    altered getSurvey loading bar code because couldn't source function on it's own

--- getSurvey.R
+++ getSurvey.R
@@ -2,6 +2,17 @@
 # base::source all files using lapply()
 
 # Call the loading animation function before fetch_survey
+show_loading_animation <- function() {
+  cat("Loading ")
+  pb <- txtProgressBar(min = 0, max = 20, style = 3)
+  
+  for (i in 1:20) {
+    Sys.sleep(0.1) # Simulate some computation time
+    setTxtProgressBar(pb, i)
+  }
+  
+  close(pb)
+}
 show_loading_animation()
 
 getSurvey <- function(qualtrics_alias, identifier = "src_subject_id", label = FALSE) {
